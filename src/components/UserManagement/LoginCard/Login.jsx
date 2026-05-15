24a2254984485721726f25d1bd44f857037756c6
GabrielDominguesSantos <gab.domiingues@gmail.com>
2026-05-15 20:18:36 -0300

    feat/Responsividade para desktops de resolução mais baixa

--- src/components/UserManagement/LoginCard/Login.jsx
+++ src/components/UserManagement/LoginCard/Login.jsx
@@ -7,7 +7,7 @@ import { BsEye, BsEyeFill } from 'react-icons/bs';
 function LoginCard(){
     return(
         <div className="contentContainer">
-            <img src={logoCronos} alt="Logo-Cronos" style={{width: '50%'}}/>
+            <img src={logoCronos} alt="Logo-Cronos" style={{width: '50%', position: 'absolute'}}/>
             <div className="header">
                 <h6 style={{fontSize: '0.8rem'}}>COMEÇE DE GRAÇA</h6>
                 <h2 style={{color: 'black', fontFamily: 'Archivo', fontWeight: '700', fontSize: '1.5rem'}}>Crie uma nova conta</h2>
